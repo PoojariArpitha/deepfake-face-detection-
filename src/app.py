92179f07bf0c15e2f23420805eecec02999b084d
PoojariArpitha <poojariarpitha81@gmail.com>
2026-04-14 19:38:51 +0530

    moved app.py to src folder

--- src/app.py
+++ src/app.py
@@ -1,1 +1,18 @@
 
+import streamlit as st
+
+st.set_page_config(page_title="Deepfake Detection", layout="centered")
+
+st.title("AI Based Face Swap Detection")
+
+video = st.file_uploader("Upload Video", type=["mp4", "avi", "mov"])
+
+if video is not None:
+    st.video(video)
+
+    if st.button("Check video"):
+        result = "Fake"
+        confidence = 90
+
+        st.write("Prediction:", result)
+        st.write("Accuracy Level:", str(confidence) + "%")
